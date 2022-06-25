e2d45695f932474fb861c9f727c578594119176b
RodrigofrBastos <rodrigo.bastos@aln.senaicimatec.edu.br>
2022-06-25 02:55:50 -0300

    ainda n consigo ler a placa

--- desafio_VC_RAS/testes.py
+++ desafio_VC_RAS/testes.py
@@ -8,73 +8,56 @@ import os
 import random
 
 
-'''
-img = cv.imread('desafio_VC_RAS/images/placaARG.jpeg')
-imgResize = cv.resize(img, (962,623))
-cv.imshow("Color",imgResize)
+#           TIPOS DE FILTROS PARA AS IMAGENS
 
-grey = cv.cvtColor(imgResize,cv.COLOR_BGR2GRAY)
-cv.imshow("grey",grey)
-
-blur = cv.GaussianBlur(imgResize,(7,7),cv.BORDER_DEFAULT)
-cv.imshow("Blurred",blur)
-
-canny = cv.Canny(imgResize,125,175)
-cv.imshow("Edge Cascaded image",canny)
-
-dilated = cv.dilate(canny,(7,7),iterations=3)
-cv.imshow("dilated images",dilated)
-
-imgHSV = cv.cvtColor(imgResize, cv.COLOR_BGR2HSV)
-cv.imshow("imgHSV", imgHSV)
-
-cv.waitKey(0)
-
-'''
-
-
-
-
-img = cv.imread('desafio_VC_RAS/images/A-placa-preta-pode-voltar-em-2021-para-carros-de-colecao.png')
-
-
-imgResize = cv.resize(img, (962,623))
+# img = cv.imread('desafio_VC_RAS/images/placaARG.jpeg')
+# imgResize = cv.resize(img, (962,623))
+# cv.imshow("Color",imgResize)
 
 # grey = cv.cvtColor(imgResize,cv.COLOR_BGR2GRAY)
-# blur = cv.GaussianBlur(grey,(7,7),cv.BORDER_DEFAULT)
-# canny = cv.Canny(blur,125,175)
-# dilated = cv.dilate(canny,(7,7),iterations=3)
-# thresh = 255 - cv.threshold(dilated, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+# cv.imshow("grey",grey)
 
-extractedInformation = pytesseract.image_to_string(img)
+# blur = cv.GaussianBlur(imgResize,(7,7),cv.BORDER_DEFAULT)
+# cv.imshow("Blurred",blur)
+
+# canny = cv.Canny(imgResize,125,175)
+# cv.imshow("Edge Cascaded image",canny)
+
+# dilated = cv.dilate(canny,(7,7),iterations=3)
+# cv.imshow("dilated images",dilated)
+
+# imgHSV = cv.cvtColor(imgResize, cv.COLOR_BGR2HSV)
+# cv.imshow("imgHSV", imgHSV)
+
+kernel = np.ones((2,2),np.uint8)
+
+img = cv.imread('desafio_VC_RAS/images/placaARG.jpeg')
+imgResize = cv.resize(img, (962,623))
+
+imgHSV = cv.cvtColor(imgResize, cv.COLOR_BGR2HSV)
+grey = cv.cvtColor(imgHSV,cv.COLOR_BGR2GRAY)
+
+
+h_min = 0
+h_max = 360
+s_min = 0
+s_max = 255
+v_min = 0
+v_max = 50
+
+lower = np.array([h_min,s_min,v_min])
+upper = np.array([h_max,s_max,v_max])
+
+# Criação de máscara com base nos limites estabelecidos
+mask = cv.inRange(imgHSV,lower,upper)
+canny = cv.Canny(mask,125,175)
+blur = cv.GaussianBlur(canny,(7,7),cv.BORDER_DEFAULT)
+imgEroded = cv.erode(blur,kernel,iterations=1)
+thresh = 255 - cv.threshold(imgEroded, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
+
+extractedInformation = pytesseract.image_to_string(thresh)
 print('placa: ',extractedInformation)
 
-cv.imshow("thresh",img)
-
+cv.imshow("thresh",thresh)
 
 cv.waitKey(0)
-
-'''
-# Transforma em escala de cinza
-gray = cv.cvtColor(imgResize, cv.COLOR_BGR2GRAY)
-thresh = 255 - cv.threshold(gray, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)[1]
-
-# Aplica blur
-thresh = cv.GaussianBlur(thresh, (3,3), 0)
-
-# Realiza a extração de texto
-placa = pytesseract.image_to_string(thresh)
-print("placa é: ",placa)
-
-cv.imshow("img",thresh)
-cv.waitKey(0)
-'''
-
-
-
-
-
-
-
-
-
